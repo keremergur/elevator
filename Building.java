8c3a5fd2345664c09ec3410d758f3d6a634ee118
Kerem Ergür <ergur.kerem@gmail.com>
2023-03-16 21:39:41 +0300

    lowest floor

--- Building.java
+++ Building.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 public class Building {
 
+    int lowestFloorNo;
     int numberOfFloors; // Including ground floor
     int numberOfElevators;
 
-    List<Floor> floors = Initializer.initializeFloors(numberOfFloors);
+    List<Floor> floors = Initializer.initializeFloors(numberOfFloors, lowestFloorNo);
     List<Elevator> elevators = Initializer.initializeElevators(numberOfElevators);
 
     public Building(int numberOfFloors, int numberOfElevators){
